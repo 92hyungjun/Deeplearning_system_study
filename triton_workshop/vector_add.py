e3d5ae0f4ee851c7b02b21b274b784b9dc46f9be
92hyungjun <78071764+92hyungjun@users.noreply.github.com>
2024-12-23 10:36:44 +0900

    Update vector_add.py

--- triton_workshop/vector_add.py
+++ triton_workshop/vector_add.py
@@ -4,11 +4,11 @@ import triton
 import triton.language as tl
 
 @triton.jit
-def add_kernel(x_ptr,  # *Pointer* to first input vector.
-               y_ptr,  # *Pointer* to second input vector.
-               output_ptr,  # *Pointer* to output vector.
-               n_elements,  # Size of the vector.
-               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+def add_kernel(x_ptr,  
+               y_ptr,  
+               output_ptr,  
+               n_elements,  
+               BLOCK_SIZE: tl.constexpr, 
                ):
     pid = tl.program_id(axis=0) 
     block_start = pid * BLOCK_SIZE
@@ -25,7 +25,8 @@ def add(x: torch.Tensor, y: torch.Tensor):
     def grid(meta):
         g_size = (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
         return g_size
-    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=32, num_warps=1, num_stages=1)
+    add_kernel[grid](x, y, output, n_elements, 
+                    BLOCK_SIZE=32, num_warps=1, num_stages=1)
     return output
 
 input_shapes = (128,1)
